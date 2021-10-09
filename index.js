7191c6a76e6876a4bf554744fcf043b0e28fc399
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-10-09 16:15:27 +0300

    code review

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  minutes: document.querySelector('span[data-value="mins"]'),
+  seconds: document.querySelector('span[data-value="secs"]'),
+};
+
+console.log(refs);
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
@@ -5,33 +13,26 @@ class CountdownTimer {
   }
 
   start() {
-    let lastDate=Date.now();
-     const timer = document.querySelector(this.selector);
-     const fields = timer.children;
-    console.log(timer.children)
-
+    let lastDate = Date.now();
     const x = setInterval(() => {
-      
       const dateNow = Date.now();
       const datesDiff = this.targetDate - dateNow;
-      if (datesDiff<0) {
+      if (datesDiff < 0) {
         clearInterval(x);
         document.getElementById("timer-1").innerHTML = "EXPIRED";
       }
       const { days, hours, mins, secs } = getTimeComponents(datesDiff);
-      updateClockFace({ days, hours, mins, secs }, this.selector);
+      updateClockFace({ days, hours, mins, secs }, refs);
     }, 1000);
   }
 }
-
-
 
 const ct = new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Oct 22, 2021"),
 });
 
-ct.start()
+ct.start();
 
 function getTimeComponents(time) {
   const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -47,90 +48,9 @@ function padTime(value) {
   return String(value).padStart(2, "0");
 }
 
-// function padDays(value) {
-//   return String(value).padStart(3, "0");
-// }
-
-const updateClockFace = ({ days, hours, mins, secs }, selector) => {
-  const fields = document.querySelector(selector).children;
-  [...fields].forEach((field) => {
-    const spans = field.children;
-    [...spans].forEach((element) => {
-      const attrDatasetValue = element.dataset.value;
-     
-      if (attrDatasetValue === "mins") {
-        element.parentNode.querySelector(".value").textContent = `${mins}`;
-      }
-      if (attrDatasetValue === "days") {
-        element.parentNode.querySelector(".value").textContent = `${days}`;
-      }
-      if (attrDatasetValue === "hours") {
-        element.parentNode.querySelector(".value").textContent = `${hours}`;
-      }
-      if (attrDatasetValue === "secs") {
-        element.parentNode.querySelector(".value").textContent = `${secs}`;
-      }
-    });
-  });
+const updateClockFace = ({ days, hours, mins, secs }, references) => {
+  references.days.textContent = `${days}`;
+  references.hours.textContent = `${hours}`;
+  references.minutes.textContent = `${mins}`;
+  references.seconds.textContent = `${secs}`;
 };
-
-const getProp = (propName, selector) => {
-  const fields = document.querySelector(selector).children;
-  
-  [...fields].forEach((field) => {
-    const spans = field.children;
-    
-    [...spans].forEach((element) => {
-      
-      const attrDatasetValue = element.dataset.value;
-     
-      if (attrDatasetValue === propName) {
-        console.log(attrDatasetValue, propName)
-        console.log(attrDatasetValue === propName)
-        console.log(element.parentNode.parentNode)
-       element.innerHTML;
-      }
-      // if (attrDatasetValue === "days") {
-      //   return element.parentNode.querySelector(".value");
-      // }
-      // if (attrDatasetValue === "hours") {
-      //   return element.parentNode.querySelector(".value");
-      // }
-      // if (attrDatasetValue === "secs") {
-      //   return element.parentNode.querySelector(".value");
-      // }
-    });
-  });
-};
-
-
-// const animate = (dateNow, lastDate, selector) => {
-// const dateNowObj = new Date(dateNow);
-// const lastDateObj = new Date(lastDate);
-
-// if (dateNowObj.getSeconds() !== lastDateObj.getSeconds()) {
-
-// }
-//   if (dateNow !== lastDate) {
-//     console.log(dateNowObj.getMonth(), lastDateObj.getDay());
-//     console.log(document.querySelectorAll(".value"))
-//     document.querySelectorAll(".value").forEach(element => {
-//       element.classList.add('animate__fadeOut');
-//     });
-    
-//     setTimeout(function(){
-//       document.querySelectorAll(".value").forEach(element => {
-//         element.classList.remove('animate__fadeOut');
-//       })
-    
-//     }, 500);
-//   }
-// }
-
-// const animate = (dateNow, lastDate, selector) => {
-
-//   const dateNowObj = new Date(dateNow);
-//   const lastDateObj = new Date(lastDate);
-
-//   if (dateNowObj.getSeconds() !==  lastDateObj)
-// }
